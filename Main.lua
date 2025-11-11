a190ca21077c15dc13a269b87647b150b7356b1f
Virskay <onpoko03@gmail.com>
2025-11-11 18:04:26 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,7 @@
 --// File handling configuration 
 local FilesConfig = {
 	UseWorkspace = false,
-	RepoUrl = "https://raw.githubusercontent.com/Virskay/Spy-Reupload" -- "http://127.0.0.1:3000"
+	RepoUrl = "https://raw.githubusercontent.com/Virskay/Spy-Reupload/Main.lua" -- "http://127.0.0.1:3000"
 }
 
 --// Service handlers
